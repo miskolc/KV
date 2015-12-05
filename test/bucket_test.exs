6386ed6e66d510eb37198c08d208c6111247198a
Dragos C <calinho_gx1992@yahoo.com>
2015-12-05 21:44:25 +0200

    Add delete/1 method

--- test/bucket_test.exs
+++ test/bucket_test.exs
@@ -12,4 +12,10 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "deletes values by key", %{test_context: bucket} do
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.delete(bucket, "milk") == 3
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
 end
